b58780258ded28d54b2e96efb2b6bc822b558230
LangRi <18510444551@163.com>
2020-01-03 17:42:56 +0800

    新增循环队列实现

--- src/main/java/com/langri/datastructure/queue/LoopQueue.java
+++ src/main/java/com/langri/datastructure/queue/LoopQueue.java
@@ -1,6 +1,5 @@
 package com.langri.datastructure.queue;
 
-import org.omg.CORBA.Object;
 
 /**
  * @author LR
@@ -14,7 +13,7 @@ public class LoopQueue<E> implements Queue<E> {
     private int size;
 
     public LoopQueue(int capacity){
-        data = (E[])new Object[capacity - 1];
+        data = (E[])new Object[capacity + 1];
         front = 0;
         tail = 0;
         size = 0;
@@ -37,16 +36,85 @@ public class LoopQueue<E> implements Queue<E> {
     }
 
     public E getFront() {
-        return null;
+        if(isEmpty()){
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return data[front];
     }
 
 
     public void enqueue(E e) {
+        if((tail + 1) % data.length == front){
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail +1) % data.length;
+        size ++;
+    }
 
+    private void resize(int newCapacity){
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for (int i = 0; i < size; i ++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
     }
 
     public E dequeue() {
-        return null;
+        if(isEmpty()){
+            throw new IllegalArgumentException("cannot dequeue form an empty queue");
+        }
+        E e = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size --;
+
+        if(size == getCapacity()/4 && getCapacity()/2 != 0 ){
+            resize(getCapacity()/2);
+        }
+
+        return e;
     }
 
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size =%d , capacity =%d\n", size, getCapacity()));
+        res.append(" front [");
+        for (int i = front; i != tail; i=(i + 1)% data.length) {
+            res.append(data[i]);
+            if((i+1)%data.length != tail){
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LoopQueue<Integer> loopQueue =new LoopQueue<Integer>();
+        for (int i = 0; i < 10 ; i++) {
+            loopQueue.enqueue(i);
+            System.out.println(loopQueue);
+            if(i%3 == 2){
+                loopQueue.dequeue();
+                System.out.println(loopQueue);
+            }
+        }
+
+            /*LoopQueue<Integer> queue = new LoopQueue<Integer>();
+            for(int i = 0 ; i < 10 ; i ++){
+                queue.enqueue(i);
+                System.out.println(queue);
+
+                if(i % 3 == 2){
+                    queue.dequeue();
+                    System.out.println(queue);
+                }
+            }*/
+
+
+    }
 }

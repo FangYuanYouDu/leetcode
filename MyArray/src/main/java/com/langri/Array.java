076eab78c93ae73a9340b97125cacc06c71814fe
LangRi <18510444551@163.com>
2020-06-22 19:57:15 +0800

    新增array数组的添加修改代码

--- MyArray/src/main/java/com/langri/Array.java
+++ MyArray/src/main/java/com/langri/Array.java
@@ -1,3 +1,5 @@
+package com.langri;
+
 /**
  * @author FangYuan
  * 2020/6/19 15:28
@@ -46,5 +48,36 @@ public class Array {
      */
     public boolean isEmpty(){
         return size == 0;
+    }
+
+    public void addFirst(int e){
+        add(0, e);
+    }
+
+    public void addLast(int e){
+        /*if(size == data.length){
+            throw new IllegalArgumentException("addLast fail. array is full");
+        }
+        data[size] = e;
+        size++;*/
+        add(size, e);
+    }
+    public void add(int index,int e){
+        if(size == data.length){
+            throw new IllegalArgumentException("add fail. array is full");
+        }
+
+        if(index > 0 || index > size){
+            throw new IllegalArgumentException("add fail. index is illegal with index > 0 || index > size");
+        }
+
+        /*for (int i = size; i > index; i--){
+            data[i] = data[i-1];
+        }*/
+        for(int i = size-1; i >= index; i--){
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size ++;//等效于 size = size + 1;
     }
 }
